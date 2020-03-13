3c3dafad361ad49c28e97f5b112de6972e839f51
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-03-13 14:53:15 +0530

    gave birthYear-> 2020

--- task/src/bot/SimpleBot.java
+++ task/src/bot/SimpleBot.java
@@ -3,6 +3,7 @@ package bot;
 class SimpleBot {
     public static void main(String[] args) {
         System.out.println("Hello! My name is Sam.");
+        System.out.println("I was created in 2020.");
     }
 
 }
